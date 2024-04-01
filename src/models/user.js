919491a40b53dbd610189bc611de0feb7bad871e
GuzmanDynastie LINUX <guzmanjrpro@gmail.com>
2024-04-01 01:29:15 -0600

    Have been added the user.js model
    Have been created the schema for mongoDB

---
+++ src/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    image: { type: String, required: false },
+    role: { type: String, required: true }
+});
+
+module.exports = mongoose.model('users', userSchema);
