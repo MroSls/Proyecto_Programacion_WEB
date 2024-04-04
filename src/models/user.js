219068f3c7aeae95552027602fa28aae73ec5a47
GuzmanDynastie LINUX <guzmanjrpro@gmail.com>
2024-04-04 17:04:31 -0600

    Have added maxlength and default values for some values

--- src/models/user.js
+++ src/models/user.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name: { type: String, required: true },
-    surname: { type: String, required: true },
-    email: { type: String, required: true },
+    name: { type: String, required: true, maxlength: 50},
+    surname: { type: String, required: true, maxlength: 50 },
+    email: { type: String, required: true, maxlength: 50 },
     password: { type: String, required: true },
-    image: { type: String, required: false },
-    role: { type: String, required: true }
+    image: { type: String, required: false , default: ''},
+    role: { type: String, required: true, default: 'user' },
+    status: { type: Boolean, required: true, default: true }
 });
 
 module.exports = mongoose.model('users', userSchema);
