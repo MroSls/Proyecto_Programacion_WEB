a94b6008b37af79625b4e0e1826c867ad11131c3
GuzmanDynastie LINUX <guzmanjrpro@gmail.com>
2024-04-05 00:12:38 -0600

    database.js relocated

--- src/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/NutriPet-Healty')
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.log(err));
