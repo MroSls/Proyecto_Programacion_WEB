bddc162a60b985ec459d9469469302de7cd00429
GuzmanDynastie LINUX <guzmanjrpro@gmail.com>
2024-04-05 00:04:57 -0600

    Array type values were added

--- src/models/product.js
+++ src/models/product.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
-    brand: { type: String, required: true , maxlength: 50},
-    title: { type: String, required: true , maxlength: 50},
-    animalCategory: { type: String, required: true , maxlength: 50},
-    productCategory: { type: String, required: true , maxlength: 50},
-    description: { type: String, required: true , maxlength: 500},
     images: [{ type: String, required: true }],
     piece: { type: Number, required: true },
     stock: { type: Number, required: true },
-    kilogram: { type: Number, required: true },
     pricePerUnit: { type: Number, required: true },
     expiration: { type: Date, required: true },
+    principalCharacteristics: { type: String, required: false, default: ''},
+    petCharacteristics: [{ type: String, required: false, default: '' }],
+    specifications: [{ type: String, required: false, default: '' }],
+    generalCharacteristics: [{ type: String, required: false, default: '' }],
+    others: [{ type: String, required: false, default: '' }],
+    description: { type: String, required: true },
     status: { type: Boolean, required: true, default: true }
 });
 
